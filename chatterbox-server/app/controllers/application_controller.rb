40b44d385c5e714e79c1abd3aafea71c1d6da875
Lynden Lim <lyndenlim15@gmail.com>
2022-03-03 21:44:57 -0500

    commit tree 221fb240fde2ece130daf95c4b908bf138726b9a

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  # Add routes
-  
+  set :default_content_type, "application/json"
+
+  get "/messages" do
+    messages = Message.all.order(message: :asc)
+    messages.to_json
+  end
+
+  post "/messages" do
+    message = Message.create(body: params[:body], username: params[:username])
+    message.to_json
+  end
+
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+    message.update(body: params[:body])
+    message.to_json
+  end
+
+  delete "/messages/:id" do
+    message = Message.find(params[:id]).destroy
+    message.to_json
+  end
 end
